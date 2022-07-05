edfc0211600903b8cff4f6ec139c8f0d343bc8e8
Robert <107894728+ivytechone@users.noreply.github.com>
2022-07-05 16:02:49 -0700

    Dockerize (#4)
    * add dockerfile
    
    * add docker image
    
    * fix docker login
    
    * fix docker push
    
    * fixing test
    
    * clean up
    
    * Change targetpath
    
    * don't tag latest
    
    * Update Dockerfile
    
    * Update Dockerfile

--- IvyAuthTest/testhelper.js
+++ IvyAuthTest/testhelper.js
@@ -1,18 +1,27 @@
 const bindir=process.env.IVYAUTH_BINDIR;
-const fs = require('fs')
+const fs = require('fs-extra')
 const axios = require('axios');
 const {spawn} = require('child_process');
-const assert = require('assert')
+const assert = require('assert');
+const { VerifierAsync } = require('fast-jwt');
 
 var serverProcess = null
 
 function startServerWithSettings(settings) {
+    const targetDir = '/tmp/ivyAuthTestRun';
+
     describe('StartServer', function(){
+        it ('Setup test run dir', async function() {
+            assert.ok(await setupTestRunDir(targetDir));
+        });
+        it ('Copy binaries', async function () {
+            assert.ok(await copyBins(targetDir));
+        });
         it ('Copy test settings', async function () {
-          assert.ok(await copySettings('testsettings.json'));
+          assert.ok(await copySettings('testsettings.json', targetDir));
         });
         it ('Start server', async function () {
-          assert.ok(await launchServer());
+          assert.ok(await launchServer(targetDir));
         });
       });
 }
@@ -39,31 +48,61 @@ async function makeRequest(requestBody) {
     });
 }
 
-function copySettings(settings) {
+function setupTestRunDir(targetDir)
+{
+    return new Promise(resolve => {
+        fs.emptyDir(targetDir)
+        .then(() => {
+            resolve(true)
+        })
+        .catch( err => {
+            console.log('EmptyDir Error:', err);
+            resolve(false);
+        });
+    });
+}
+
+function copyBins(targetDir) {
     return new Promise(resolve => {
         if (bindir == undefined || bindir.length ==0) {
             console.log ('CopySettings Error: IVYAUTH_BINDIR not set.');
             resolve(false);
         }
         else {
-            fs.copyFile(settings, bindir + '/appsettings.json', (err) => {
-                if(err)
-                {
-                    console.log('CopySettings Error: ', err);
-                    resolve(false)
-                }
-                else
-                {
-                    resolve(true);
-                }
+            fs.copy(bindir, targetDir)
+            .then(() => {
+                resolve(true);
+            })
+            .catch( err => {
+                console.log('CopyBins Error:', err);
+                resolve(false);
             });
         }
     });
 }
 
-function launchServer(settings) {
+function copySettings(settingsFile, targerDir) {
     return new Promise(resolve => {
-        serverProcess = spawn(bindir + '/IvyAuth', [], {'cwd': bindir})
+        if (bindir == undefined || bindir.length ==0) {
+            console.log ('CopySettings Error: IVYAUTH_BINDIR not set.');
+            resolve(false);
+        }
+        else {
+            fs.copyFile(settingsFile, targerDir + '/appsettings.json')
+            .then(() => {
+                resolve(true);
+            })
+            .catch(err => {
+                console.log("Copy Settings Error: ", err);
+                resolve(false);
+            });
+        }
+    });
+}
+
+function launchServer(targetDir) {
+    return new Promise(resolve => {
+        serverProcess = spawn(targetDir + '/IvyAuth', [], {'cwd': targetDir})
 
         serverProcess.stdout.on('data', (data) => {
             console.log(`server stdout: [[${data}]]`);
