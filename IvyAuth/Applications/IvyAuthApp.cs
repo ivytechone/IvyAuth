a3a0a165c12109fe91e2f740b605aabc63460dde
Robert <107894728+ivytechone@users.noreply.github.com>
2022-07-05 11:59:23 -0700

    inheritence

--- IvyAuth/Applications/IvyAuthApp.cs
+++ IvyAuth/Applications/IvyAuthApp.cs
@@ -1,6 +1,8 @@
+using IvyAuth.Interfaces;
+
 namespace IvyAuth.Applications
 {
-    public class IvyAuthApp
+    public class IvyAuthApp : IApplication
     {
         public string Id => "00000000-0000-0000-0000-000000000001";
     }
