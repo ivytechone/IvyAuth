a688504cc29e67feee5b31926902759244ee137d
Robert <robert@ivytech.one>
2022-07-16 08:06:24 -0700

    add logging

--- IvyAuth/Controllers/AnonymousIdController.cs
+++ IvyAuth/Controllers/AnonymousIdController.cs
@@ -1,5 +1,6 @@
 using IvyAuth.Extensions;
 using IvyAuth.Interfaces;
+using IvyTech.Logging;
 using JWT.Algorithms;
 using JWT.Builder;
 using Microsoft.AspNetCore.Mvc;
@@ -24,18 +25,22 @@ namespace IvyAuth.Controllers
         [Route("api/anonymousid/")]
         public IActionResult Get()
         {
+            const string apiName = "GetAnonymousId";
             var appId = HttpContext.GetApplicationId();
             var timeZone = HttpContext.GetTimeZone();
             var app = _applicationManager.GetAppById(appId);
 
             if (app is null)
             {
+                _logger.LogWarning("{apiname}: {msg} {appid}", apiName, "x-application-id invalid or missing", appId);
+                this.HttpContext.GetRequestLoggerContext().Diag = "invalidAppId";
                 return new BadRequestObjectResult("x-application-id invalid or missing");
             }
 
             // TimeZone is optional default to UTC
             if (timeZone is null)
             {
+                _logger.LogInformation("{apiname}: {msg}", apiName, "Using default timezone");
                 timeZone = "Etc/UTC";
             }
             // Check if user provided Windows Format, Convert and use IANA format
@@ -46,20 +51,26 @@ namespace IvyAuth.Controllers
             // Check if user provided valid IANA timezone
             else if (!TimeZoneInfo.TryConvertIanaIdToWindowsId(timeZone, out string? windowsId))
             {
-                    return new BadRequestObjectResult("x-timezone contains invalid time zone");
+                _logger.LogWarning("{apiname}: {msg}", apiName, "x-timezone contains invalid time zone");
+                this.HttpContext.GetRequestLoggerContext().Diag = "invalidTimeZone";
+                return new BadRequestObjectResult("x-timezone contains invalid time zone");
             }
 
             var cert = _certificateManager.GetAidCertificateWithPrivateKey();
+            var identity = Guid.NewGuid().ToString();
 
             var token = JwtBuilder.Create()
                 .WithAlgorithm(new RS256Algorithm(cert))
                 .AddClaim("iat", DateTimeOffset.UtcNow.ToUnixTimeSeconds())
                 .AddClaim("iss", "ivytech.one")
-                .AddClaim("sub", Guid.NewGuid().ToString().ToUpperInvariant())
+                .AddClaim("sub", identity)
                 .AddClaim("aud", app.Id)
                 .AddClaim("zoneinfo", timeZone)
                 .Encode();
 
+            this.HttpContext.GetRequestLoggerContext().Identity = identity;
+
+            _logger.LogInformation("{apiname}: Success {timezone} {app}", apiName, timeZone, app.Id);
             return new OkObjectResult(token);
         }
     }
