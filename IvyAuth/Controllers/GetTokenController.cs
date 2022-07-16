3cf906eecc301e69cc5027af94157e386461ab29
Robert <107894728+ivytechone@users.noreply.github.com>
2022-07-16 07:44:33 -0700

    Logging (#8)
    * Connect serilog to elasticsearch
    
    * RequestLogging
    
    * Finalize Request Logger
    
    * turn off autoregister template
    
    * finalize
    
    * remove unsed file
    
    * adding debug log
    
    * Refactor
    
    * Don't require logging
    
    * move away from global LogContext
    
    * Bugfixes
    
    * App logging context
    
    * minlevel
    
    * add request logger context
    
    * request tag
    
    * add request log enrichers

--- IvyAuth/Controllers/GetTokenController.cs
+++ IvyAuth/Controllers/GetTokenController.cs
@@ -3,6 +3,7 @@ using IvyAuth.Interfaces;
 using JWT.Algorithms;
 using JWT.Builder;
 using Microsoft.AspNetCore.Mvc;
+using IvyTech.Logging;
 
 namespace IvyAuth.Controllers
 {
@@ -10,15 +11,14 @@ namespace IvyAuth.Controllers
 	[Route("api/generatetoken/")]
 	public class GenerateTokenController : ControllerBase
 	{
-		private readonly ILogger<GenerateTokenController> _logger;
+		//private readonly ILogger<GenerateTokenController> _logger;
 		private readonly ICertificateManager _certificateManager;
 		private readonly IApplicationManager _applicationManager;
 		private readonly IIdentityStore _identityStore;
 
 		public GenerateTokenController(ILogger<GenerateTokenController> logger, ICertificateManager certificateManager, IIdentityStore identityStore, IApplicationManager applicationManager)
 		{
-			logger.LogInformation("Initializing TokenController");
-			_logger = logger;
+			//_logger = logger;
 			_certificateManager = certificateManager;
 			_applicationManager = applicationManager;
 			_identityStore = identityStore;
@@ -27,47 +27,39 @@ namespace IvyAuth.Controllers
 		[HttpPost]
 		public IActionResult Post([FromBody] UserNamePassword creds)
 		{
-			using (_logger.BeginScope("GenerateToken"))
+			var requestLogContext = HttpContext.GetRequestLoggerContext();
+
+			if (creds == null)
 			{
-				try
-				{
-					if (creds == null)
-					{
-						_logger.LogWarning("Failed to parse input");
-						return new UnauthorizedResult();
-					}
-
-					var identity = _identityStore.Authenticate(creds);
-
-					if (identity == null)
-					{
-						_logger.LogWarning("Failed authentication for {username}", creds.UserName);
-						return new UnauthorizedResult();
-					}
-
-					var cert = _certificateManager.GetCertificateWithPrivateKey();
-					var app = _applicationManager.IvyAuthApp;
-
-					var token = JwtBuilder.Create()
-					  .WithAlgorithm(new RS256Algorithm(cert))
-					  .AddClaim("exp", DateTimeOffset.UtcNow.AddDays(7).ToUnixTimeSeconds())
-					  .AddClaim("iss", "ivytech.one")
-					  .AddClaim("aud", app.Id)
-					  .AddClaim("sub", identity.Id)
-					  .AddClaim("name", identity.Name)
-					  .AddClaim("scopes", "")
-					  .AddClaim("zoneinfo", identity.TimeZone)
-					  .Encode();
-
-					_logger.LogInformation("Successful authentication");
-					return new OkObjectResult(token);
-				}
-				catch (Exception ex)
-				{
-					_logger.LogError(ex, "Unexpected Exception");
-					return StatusCode(500);
-				}
+				requestLogContext.Diag = DiagCodes.BadRequest;
+				return new UnauthorizedResult();
 			}
+
+			var identity = _identityStore.Authenticate(creds);
+
+			if (identity == null)
+			{
+				requestLogContext.Diag = DiagCodes.AuthFailed;
+				return new UnauthorizedResult();
+			}
+
+			requestLogContext.Identity = identity.Id;
+
+			var cert = _certificateManager.GetCertificateWithPrivateKey();
+			var app = _applicationManager.IvyAuthApp;
+
+			var token = JwtBuilder.Create()
+				.WithAlgorithm(new RS256Algorithm(cert))
+				.AddClaim("exp", DateTimeOffset.UtcNow.AddDays(7).ToUnixTimeSeconds())
+				.AddClaim("iss", "ivytech.one")
+				.AddClaim("aud", app.Id)
+				.AddClaim("sub", identity.Id)
+				.AddClaim("name", identity.Name)
+				.AddClaim("scopes", "")
+				.AddClaim("zoneinfo", identity.TimeZone)
+				.Encode();
+
+			return new OkObjectResult(token);
 		}
 	}
 }
--- IvyAuth/Program.cs
+++ IvyAuth/Program.cs
@@ -1,12 +1,30 @@
 using IvyAuth;
 using IvyAuth.Config;
 using IvyAuth.Interfaces;
+using IvyTech.Logging;
+using Serilog;
+
+// App level logging context
+IvyTech.Logging.AppContext.SetAppName("IvyAuth");
+IvyTech.Logging.AppContext.SetVersion(AppInfo.Version);
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IIdentityStore>(x => new StaticIdentityStore(builder.Configuration.GetSection("StaticIdentityStore").Get<StaticIdentityStoreConfig>()));
-builder.Services.AddSingleton<ICertificateManager>(x => new StaticCertManager(builder.Configuration.GetSection("StaticCertManager").Get<StaticCertManagerConfig>()));
-builder.Services.AddSingleton<IApplicationManager>(x => new ApplicationManager());
-builder.Services.AddControllers();
-var app = builder.Build();
-app.MapControllers();
-app.Run();
+var logger = DebugLogger.CreateLogger(builder.Configuration);
+
+try
+{
+	builder.Services.AddHttpContextAccessor();
+	builder.Services.AddLogging(x => x.AddSerilog(logger));
+	builder.Services.AddSingleton<IIdentityStore>(x => new StaticIdentityStore(builder.Configuration.GetSection("StaticIdentityStore").Get<StaticIdentityStoreConfig>()));
+	builder.Services.AddSingleton<ICertificateManager>(x => new StaticCertManager(builder.Configuration.GetSection("StaticCertManager").Get<StaticCertManagerConfig>()));
+	builder.Services.AddSingleton<IApplicationManager>(x => new ApplicationManager());
+	builder.Services.AddControllers();
+	var app = builder.Build();
+	app.MapControllers();
+	app.UseIvyLogging();
+	app.Run();
+}
+catch (Exception ex)
+{
+	logger?.Fatal("Unhandled Excpetion", ex);
+}
