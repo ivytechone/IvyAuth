1328278779ad8094a1ddc3653a3a170c4d6314ee
Robert <robert@ivytech.one>
2022-07-23 08:54:44 -0700

    Use logging package

--- IvyLogging/DiagCodes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IvyTech.Logging
-{
-	public class DiagCodes
-	{
-		public static string UnhandledException => "UnhandledException";
-		public static string BadRequest => "BadRequest";
-		public static string AuthFailed => "AuthFailed";
-	};
-}
