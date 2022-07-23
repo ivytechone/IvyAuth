488df9f7d3a17185b305fa7f1b370501e3bb65fe
Robert <107894728+ivytechone@users.noreply.github.com>
2022-07-23 08:58:53 -0700

    Use logging package (#12)
    * Use logging package
    
    * Update build.yml

--- IvyLogging/IvyLoggingConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IvyTech.Logging
-{
-	public class IvyLoggingConfig
-	{
-		public string? ElasticSearchURL { get; set; }
-		public string? Environment { get; set; }
-		public string? ApiKey { get; set; }
-	}
-}
