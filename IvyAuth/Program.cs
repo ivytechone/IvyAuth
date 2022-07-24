d3d2f07868a8c61a93ad5a0821a034f3926607b3
Robert <robert@ivytech.one>
2022-07-24 16:30:46 -0700

    Use IvyWebApiInfo Updates

--- IvyAuth/Program.cs
+++ IvyAuth/Program.cs
@@ -1,22 +1,17 @@
 using IvyAuth;
 using IvyAuth.Config;
 using IvyAuth.Interfaces;
+using IvyTech.IvyLogging.Extensions;
 using IvyTech.IvyWebApi;
 using IvyTech.Logging;
-using Serilog;
-
-// App level logging context
-IvyTech.Logging.AppContext.SetAppName("IvyAuth");
-IvyTech.Logging.AppContext.SetVersion(AppInfo.Version);
 
 var builder = WebApplication.CreateBuilder(args);
-var logger = DebugLogger.CreateLogger(builder.Configuration);
 
 try
 {
 	builder.UseIvyWebApi("IvyAuth", AppInfo.Version);
+	builder.AddIvyDebugLogger();
 	builder.Services.AddHttpContextAccessor();
-	builder.Services.AddLogging(x => x.AddSerilog(logger));
 	builder.Services.AddSingleton<IIdentityStore>(x => new StaticIdentityStore(builder.Configuration.GetSection("StaticIdentityStore").Get<StaticIdentityStoreConfig>()));
 	builder.Services.AddSingleton<ICertificateManager>(x => new StaticCertManager(builder.Configuration.GetSection("StaticCertManager").Get<StaticCertManagerConfig>()));
 	builder.Services.AddSingleton<IApplicationManager>(x => new ApplicationManager());
@@ -29,5 +24,5 @@ try
 }
 catch (Exception ex)
 {
-	logger?.Fatal("Unhandled Excpetion", ex);
+	DebugLogger.Logger?.Fatal("Unhandled Excpetion", ex);
 }
