6b0fce8a15fa13a03493db322eae1cfafb4ae401
Robert <107894728+ivytechone@users.noreply.github.com>
2023-10-06 11:36:46 -0700

    fix typo

--- IvyAuth/ClientSide/UserNamePasswordPage.cs
+++ IvyAuth/ClientSide/UserNamePasswordPage.cs
@@ -13,8 +13,8 @@ $@"<!DOCTYPE html>
     IvyTech Login
     </head>
     <body>
-      Username: <input autocomplete='username' onClick='onChangeUsername(this.value)'/>
-      Password: <input autocomplete='current-password' type='password' onClick='onChangePassword(this.value)'/>
+      Username: <input autocomplete='username' onclick='onChangeUsername(this.value)'/>
+      Password: <input autocomplete='current-password' type='password' onclick='onChangePassword(this.value)'/>
       <button onclick='onLogin()'>Login</button>
     </body>
     <script>
